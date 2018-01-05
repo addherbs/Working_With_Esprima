e0f0fd39d9dd279be9cf927ea2e9c6f0ec1b6f89
Aditya Pandey <addherbs@gmail.com>
2018-01-05 11:02:33 +0530

    Changes.

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+// uncomment after placing your favicon in /public
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
