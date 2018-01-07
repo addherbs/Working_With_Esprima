707187302e87966ac08d6b57af0209b676681e4d
Aditya Pandey <addherbs@gmail.com>
2018-01-07 18:27:53 +0530

    Added register and login route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+
+// register route
+router.get('/register', function (req,res) {
+    res.render('register');
+});
+
+
+// login route
+router.get('/login', function (req,res) {
+    res.render('login');
+});
+
+
 module.exports = router;
