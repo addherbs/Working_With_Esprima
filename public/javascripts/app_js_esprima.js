36b27b18ed219543b8e8e516f3d8f4aac57da3e0
Aditya Pandey <addherbs@gmail.com>
2018-01-26 22:22:40 +0530

    sample declaration of variables in esprima

---
+++ public/javascripts/app_js_esprima.js
@@ -0,0 +1,261 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+
+
+/*
+{
+ */
+    "type": "Program",
+    "body": [
+    {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": "express"
+                },
+                "init": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "require"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Literal",
+                            "value": "express",
+                            "raw": "'express'"
+                        }
+                    ]
+                }
+            }
+        ],
+        "kind": "var"
+    },
+    {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": "path"
+                },
+                "init": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "require"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Literal",
+                            "value": "path",
+                            "raw": "'path'"
+                        }
+                    ]
+                }
+            }
+        ],
+        "kind": "var"
+    },
+    {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": "favicon"
+                },
+                "init": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "require"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Literal",
+                            "value": "serve-favicon",
+                            "raw": "'serve-favicon'"
+                        }
+                    ]
+                }
+            }
+        ],
+        "kind": "var"
+    },
+    {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": "logger"
+                },
+                "init": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "require"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Literal",
+                            "value": "morgan",
+                            "raw": "'morgan'"
+                        }
+                    ]
+                }
+            }
+        ],
+        "kind": "var"
+    },
+    {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": "cookieParser"
+                },
+                "init": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "require"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Literal",
+                            "value": "cookie-parser",
+                            "raw": "'cookie-parser'"
+                        }
+                    ]
+                }
+            }
+        ],
+        "kind": "var"
+    },
+    {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": "bodyParser"
+                },
+                "init": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "require"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Literal",
+                            "value": "body-parser",
+                            "raw": "'body-parser'"
+                        }
+                    ]
+                }
+            }
+        ],
+        "kind": "var"
+    },
+    {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": "index"
+                },
+                "init": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "require"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Literal",
+                            "value": "./routes/index",
+                            "raw": "'./routes/index'"
+                        }
+                    ]
+                }
+            }
+        ],
+        "kind": "var"
+    },
+    {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": "users"
+                },
+                "init": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "require"
+                    },
+                    "arguments": [
+                        {
+                            "type": "Literal",
+                            "value": "./routes/users",
+                            "raw": "'./routes/users'"
+                        }
+                    ]
+                }
+            }
+        ],
+        "kind": "var"
+    },
+    {
+        "type": "VariableDeclaration",
+        "declarations": [
+            {
+                "type": "VariableDeclarator",
+                "id": {
+                    "type": "Identifier",
+                    "name": "app"
+                },
+                "init": {
+                    "type": "CallExpression",
+                    "callee": {
+                        "type": "Identifier",
+                        "name": "express"
+                    },
+                    "arguments": []
+                }
+            }
+        ],
+        "kind": "var"
+    }
+],
+    "sourceType": "script"
+}
+
+*/
