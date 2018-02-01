4b3fd93a46d1d657458745ca5e82472fc6e05412
Aditya Pandey <addherbs@gmail.com>
2018-01-31 20:01:51 -0600

    changes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,72 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/register', function (req,res) {
+
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    var cpassword = req.body.cpassword;
+    var username = req.body.username;
+
+    //Validation of form
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('email', 'EmailID is required').notEmpty();
+    req.checkBody('email', 'Email is invalid').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('cpassword', 'Passwords should match').equals(req.body.password);
+    console.log(cpassword);
+
+    var errors= req.validationErrors();
+    if(errors){
+        res.render('register',{
+            errors:errors
+        });
+    }else{
+        var newUser = new User({
+            FirstName: name,
+            UserName: username,
+            EmailID: email,
+            Password: password
+        });
+
+
+        User.createUser(newUser, function(err, user){
+            if (err) throw err;
+            console.log(user);
+        });
+
+        req.flash('success_msg', 'You are now registered.. Login to continue');
+        res.redirect('/users/login');
+    }
+
+});
+
+
+// login route
+router.get('/login', function (req,res) {
+    res.render('login');
+});
+
+
+router.post('/login',
+    passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login',failureFlash: true}),
+    function(req, res) {
+        res.redirect('/');
+    });
+
+
+router.get('/logout', function (req,res) {
+    req.logOut();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+});
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 
@@ -130,84 +193,6 @@ passport.use(new LocalStrategy(
 ));
 
 
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function (id, done) {
-    User.getUserById(id, function (err, user) {
-        done(err, user);
-    });
-});
-
-
-// register route
-router.get('/register', function (req,res) {
-    res.render('register');
-});
-
-
-router.post('/register', function (req,res) {
-
-    var name = req.body.name;
-    var email = req.body.email;
-    var password = req.body.password;
-    var cpassword = req.body.cpassword;
-    var username = req.body.username;
-
-    //Validation of form
-    req.checkBody('name', 'Name is required').notEmpty();
-    req.checkBody('username', 'Username is required').notEmpty();
-    req.checkBody('email', 'EmailID is required').notEmpty();
-    req.checkBody('email', 'Email is invalid').isEmail();
-    req.checkBody('password', 'Password is required').notEmpty();
-    req.checkBody('cpassword', 'Passwords should match').equals(req.body.password);
-    console.log(cpassword);
-
-    var errors= req.validationErrors();
-    if(errors){
-        res.render('register',{
-            errors:errors
-        });
-    }else{
-        var newUser = new User({
-            FirstName: name,
-            UserName: username,
-            EmailID: email,
-            Password: password
-        });
-
-
-        User.createUser(newUser, function(err, user){
-            if (err) throw err;
-            console.log(user);
-        });
-
-        req.flash('success_msg', 'You are now registered.. Login to continue');
-        res.redirect('/users/login');
-    }
-
-});
-
-
-// login route
-router.get('/login', function (req,res) {
-    res.render('login');
-});
-
-
-router.post('/login',
-    passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login',failureFlash: true}),
-    function(req, res) {
-        res.redirect('/');
-    });
-
-
-router.get('/logout', function (req,res) {
-    req.logOut();
-    req.flash('success_msg', 'You are logged out');
-    res.redirect('/users/login');
-});
 
 
 module.exports = router;
