6f7c2af41bad868511be914b783f400cffce47fd
Aditya Pandey <addherbs@gmail.com>
2018-02-05 19:26:05 -0600

    changes

--- public/javascripts/users js file.js
+++ public/javascripts/users js file.js
@@ -1,25 +1,60 @@
-// JS FILE
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-});
+router.post('/register', function (req,res) {
+
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    var cpassword = req.body.cpassword;
+    var username = req.body.username;
+
+    //Validation of form
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('email', 'EmailID is required').notEmpty();
+    req.checkBody('email', 'Email is invalid').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('cpassword', 'Passwords should match').equals(req.body.password);
+    console.log(cpassword);
+
+    var errors= req.validationErrors();
+    if(errors){
+        res.render('register',{
+            errors:errors
+        });
+    }else{
+        var newUser = new User({
+            FirstName: name,
+            UserName: username,
+            EmailID: email,
+            Password: password
+        });
 
 
+        User.createUser(newUser, function(err, user){
+            if (err) throw err;
+            console.log(user);
+        });
 
-// register route
-router.get('/register', function (req,res) {
-    res.render('register');
+        req.flash('success_msg', 'You are now registered.. Login to continue');
+        res.redirect('/users/login');
+    }
+
 });
 
 
 // login route
 router.get('/login', function (req,res) {
     res.render('login');
-
 });
+
+
+router.post('/login',
+    passport.authenticate('local', {successRedirect:'/', failureRedirect:'/users/login',failureFlash: true}),
+    function(req, res) {
+        res.redirect('/');
+    });
 
 
 router.get('/logout', function (req,res) {
@@ -28,6 +63,11 @@ router.get('/logout', function (req,res) {
     res.redirect('/users/login');
 });
 
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
 
 
 // verify which button is pressed
@@ -59,17 +99,103 @@ router.post('/refreshPortalList', function (req,res) {
     });
 });
 
+// Create a portal
+router.post('/createPortal', function (req,res) {
+    var pName = req.body.pName;
+    var pPassword = req.body.pPassword;
+    var hours = req.body.hours;
+    var minutes = req.body.mins;
+    var secs = req.body.secs;
+    var message = req.body.message;
+    var count = req.body.count;
+    var owner_id = req.body.owner_id;
+    var owner_username = req.body.owner_username;
+    var owner_email = req.body.owner_email;
 
-passport.serializeUser(function (user, done) {
-    done(null, user.id);
+    var cpPassword = req.body.cpPassword;
+
+    var owner_details = {
+        "owner_id" :owner_id ,
+        "owner_username": owner_username,
+        "owner_email": owner_email
+    };
+
+    var ttl = parseInt(hours*3600) + parseInt(minutes*60) + parseInt(secs);
+    console.log("ttl: ", ttl);
+
+    console.log("Portal Name:"+ pName + "\nPortal Password:"+pPassword +"\nHours-Mins-Secs: "+ hours +"-" +minutes+ "-"+ secs +
+        "\nMessage:" + message + "\nTotal TTL: "+ ttl + "\t count: " + count);
+
+    var currentDate = new Date();
+    console.log("Current Date is: ", currentDate);
+    currentDate.setSeconds(currentDate.getSeconds() + ttl);
+    console.log("Current Date is: ", currentDate);
+
+    //Validation of form
+    req.checkBody('pName', 'Portal Name is required').notEmpty();
+    req.checkBody('pPassword', 'Portal Password is required').notEmpty();
+    req.checkBody('hours', 'Hours is required/ Otherwise enter 0/ Should be Number').notEmpty().isNumeric();
+    req.checkBody('mins', 'Minutes is required/ Otherwise enter 0/ Should be Number').notEmpty().isNumeric();
+    req.checkBody('secs', 'Seconds is required/ Otherwise enter 0/ Should be Number').notEmpty().isNumeric();
+    req.checkBody('message', 'There has to be a message').notEmpty();
+    req.checkBody('count', 'You have to enter open count/ Atleast 1/ Should be Number').notEmpty().isNumeric();
+    req.checkBody('cpPassword', 'Confirm Portal Password should match Portal Password').notEmpty().equals(req.body.pPassword);
+
+
+    var portalData = {
+        PortalName: pName,
+        PortalPassword: pPassword,
+        TTL: ttl,
+        Message: message,
+        Count: count,
+        Owner_Details: owner_details,
+        expireAt: new Date(currentDate)
+    };
+
+    var errors= req.validationErrors();
+    if(errors){
+        res.render('createPortal',{
+            errors:errors
+        });
+    }else{
+        var newPortal = new Portal(portalData);
+
+        Portal.createPortal(newPortal, function(err, portal){
+            if (err) throw err;
+        });
+
+        req.flash('success_msg', 'You have successfully created a portal.. Inform your friend to check');
+
+        res.redirect('/');
+    }
+
 });
 
-passport.deserializeUser(function (id, done) {
-    User.getUserById(id, function (err, user) {
-        done(err, user);
-    });
-});
 
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        User.getUserByUsername(username, function (err, user) {
+            if (err) throw err;
+            if (!user) {
+                return done(null, false, { message: 'Unknown user' });
+            }
+            User.comparePassword(password, user.Password, function (err, isMatch) {
+                if (err) throw err;
+                if (isMatch) {
+                    return done(null, user );
+                }
+                else {
+                    return done(null, false, { message: 'Invalid password' });
+                }
+            });
+        });
+    }
+));
+
+
+
+
+module.exports = router;
 
 /*
 {
